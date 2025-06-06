f76e94526571e10c4f8e31b260384f349723bfc5
Track1at01 <ricardoawnil@gmail.com>
2025-06-05 19:12:43 -0500

    arregle los corchetes feos

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const Nombre = document.getElementById("Nombre")
 const Apellido = document.getElementById("Apellido")
 const Contraseña = document.getElementById("Contraseña")
 
-function Validar()
-
-
-{alert("nombre: "+Nombre.value+"\nApellido: "+Apellido.value +"\nContraseña: "+Contraseña.value)}
+function Validar() {
+    alert("nombre: "+Nombre.value+"\nApellido: "+Apellido.value +"\nContraseña: "+Contraseña.value)
+}
