92c6617d6800a83fa6b4610f81692b7a7f20d115
Track1at01 <ricardoawnil@gmail.com>
2025-06-05 19:00:23 -0500

    primer cambio

---
+++ script.js
@@ -0,0 +1,8 @@
+const Nombre = document.getElementById("Nombre")
+const Apellido = document.getElementById("Apellido")
+const Contraseña = document.getElementById("Contraseña")
+
+function Validar()
+
+
+{alert("nombre: "+Nombre.value+"\nApellido: "+Apellido.value +"\nContraseña: "+Contraseña.value)}
