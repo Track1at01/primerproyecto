d04094313b314567faff541398b5e8dd1bb437f4
Track1at01 <ricardoawnil@gmail.com>
2025-06-11 19:36:52 -0500

    matenme

--- script.js
+++ script.js
@@ -7,26 +7,38 @@ const Nombreresultado = document.getElementById("respuestaNombre")
 const Apellidoresultado = document.getElementById("respuestaApellido")
 const Contraseñaresultado = document.getElementById("respuestaContraseña")
 const Edadresultado = document.getElementById("respuestaEdad")
-const formulario = document.getElementById ("formulario")
+const formulario = document.getElementById("formulario")
 
-
-function enviarFormulario() {
-    Nombreresultado.innerText = "Nombre:" + Nombre.value
-    Apellidoresultado.innerText = "Apellido:" + Apellido.value
-    Contraseñaresultado.innerText = "Contraseña:" + Contraseña.value
-    Edadresultado.innerText = "Edad:" + Edad.value
-
-    const EdadNumero = parseInt(Edad.value);
-
-if ( EdadNumero >= 18 ) {
-Edadresultado.innerText = "Es mayor de edad";
-formulario.classList.remove ("menor");
-} else {
-Edadresultado.innerText = "Es menor de edad";
-
-formulario.classList.add("menor");
+function color() {
+ document.body.classList.toggle("color")
 
 }
+
+function enviarFormulario() {
+    if (Nombre.value.trim() == "" || Apellido.value.trim() == "" || Contraseña.value.trim() == "") {
+        alert("Por favor, ingrese todos los datos");
+    } else {
+        Nombreresultado.innerText = "Nombre:" + Nombre.value
+        Apellidoresultado.innerText = "Apellido:" + Apellido.value
+        Contraseñaresultado.innerText = "Contraseña:" + Contraseña.value
+        Edadresultado.innerText = "Edad:" + Edad.value
+
+        const EdadNumero = parseInt(Edad.value);
+
+        if (EdadNumero >= 18) {
+            Edadresultado.innerText = "Es mayor de edad";
+            formulario.classList.remove("menor");
+        } else {
+            Edadresultado.innerText = "Es menor de edad";
+
+            formulario.classList.add("menor");
+
+
+
+        }
+    }
+
+
 
 
 }
